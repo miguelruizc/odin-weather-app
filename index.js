87e553bb9f915afdc9e5cfbb28bae5e7a958b1c6
miguelruizc <buumchakalaka@gmail.com>
2024-05-19 00:01:37 +0200

    Add footer

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("hello javascript")
+async function fetchWeather(location) {
+    //call WeatherAPI to get the data from the specific location
+    // Request URL example: http://api.weatherapi.com/v1/current.json
+
+    //show data in console
+
+    //show data in dome
+}
