1baf4b08c38e0a23f6eb97c532a98a830a0ee6f9
miguelruizc <buumchakalaka@gmail.com>
2024-05-19 04:04:12 +0200

    Add render function to render DOM with data

--- index.js
+++ index.js
@@ -76,20 +76,92 @@ function search(event) {
 	const value = document.querySelector('.searchInput').value;
 	event.target.reset();
 
-    // Fetch weather with input location
+	// Fetch weather with input location
 	const weatherData = fetchWeather(value);
 	weatherData
 		.then((data) => {
 			const weatherToday = new CurrentWeather(data);
-			const weatherForecastDay1 = new ForecastDay(data, 0);
+			const weatherForecastDays = new Array(3);
+			weatherForecastDays[0] = new ForecastDay(data, 0);
+			weatherForecastDays[1] = new ForecastDay(data, 1);
+			weatherForecastDays[2] = new ForecastDay(data, 2);
 
 			console.table(weatherToday);
-			console.table(weatherForecastDay1);
+			console.table(weatherForecastDays);
+			render(weatherToday, weatherForecastDays);
 		})
 		.catch((error) => {
 			console.log(error);
 		});
 }
+
+function render(weather, forecast) {
+	const city = document.querySelector('.city');
+	// TODO if name/region/country repeats, merge
+    city.textContent =
+		'' +
+		weather.locationName +
+		', ' +
+		weather.locationRegion +
+		', ' +
+		weather.locationCountry;
+
+	const date = document.querySelector('.date');
+	date.textContent = weather.localTime;
+
+	const conditionIcon = document.querySelector('.conditionIcon img');
+	conditionIcon.src = weather.conditionIcon;
+
+	const condition = document.querySelector('.condition');
+	condition.textContent = weather.conditionText;
+
+	const temperature = document.querySelector('.temperature');
+	if (celsius) temperature.textContent = weather.temperatureC + 'ºC';
+	else temperature.textContent = weather.temperatureF + 'ºF';
+
+	const feelsLikeTemperature = document.querySelector('.feelsLike :nth-child(2)');
+	if (celsius) feelsLikeTemperature.textContent = weather.feelsLikeTemperatureC + "ºC";
+    else feelsLikeTemperature.textContent = weather.feelsLikeTemperatureF + "ºF";
+
+	const humidity = document.querySelector('.humidity :nth-child(2)');
+	humidity.textContent = weather.humidity + "%";
+
+	const rainChance = document.querySelector('.rainChance :nth-child(2)');
+	rainChance.textContent = weather.rainChance + "%";
+
+	const windSpeed = document.querySelector('.windSpeed :nth-child(2)');
+	windSpeed.textContent = weather.windSpeed + "km/h";
+
+    // Forecast 3 days
+	for (let i = 1; i <= 3; i++) {
+        // Date
+		const forecastDate = document.querySelector(`.day${i} .forecastDate`);
+		forecastDate.textContent = forecast[i-1].dayOfWeek;
+
+        // Temperature
+		const forecastTemperature = document.querySelector(
+			`.day${i} .forecastTemperature`
+		);
+        if(celsius) forecastTemperature.textContent = forecast[i-1].temperatureC + "ºC";
+        else forecastTemperature.textContent = forecast[i-1].temperatureF + "ºF";
+
+        // Temperature Min
+		const forecastTemperatureMin = document.querySelector(
+			`.day${i} .forecastTemperatureMin`
+		);
+		if(celsius) forecastTemperatureMin.textContent = forecast[i-1].temperatureCMin + "ºC";
+        else forecastTemperatureMin.textContent = forecast[i-1].temperatureFMin + "ºF";
+
+        // Weather
+		const forecastWeather = document.querySelector(
+			`.day${i} .forecastWeather img`
+		);
+		forecastWeather.src = forecast[i-1].weatherIcon;
+	}
+}
+
+// Start:
+let celsius = true;
 
 const searchbar = document.getElementById('searchbar');
 searchbar.addEventListener('submit', search);
