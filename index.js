b70ddd729b8353539885543d139fa7986e966151
miguelruizc <buumchakalaka@gmail.com>
2024-05-19 02:37:44 +0200

    Implement fetchWeather function

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Current weather object
-function CurrentWeather(weatherData){
-    // Main weather variables:
+function CurrentWeather(weatherData) {
+	// Main weather variables:
 	// Location
-	this.locationName = undefined;//weatherJSON.location.name
+	this.locationName = undefined; //weatherJSON.location.name
 	this.locationRegion = undefined; //weather.JSON.location.region
 	this.locationCountry = undefined; //weatherJson.location.country
 	// Date & time
@@ -11,10 +11,10 @@ function CurrentWeather(weatherData){
 	this.conditionText = undefined; //weatherJson.current.condition.text
 	this.conditionIcon = undefined; //weather.Json.current.condition.icon (url.png)
 	// Temperature
-    this.temperatureC = undefined; //weatherJson.current.temp_c
+	this.temperatureC = undefined; //weatherJson.current.temp_c
 	this.temperatureF = undefined; //weatherJson.current.temp_f
-	
-    // Additional weather variables:
+
+	// Additional weather variables:
 	// Feels like temp
 	this.feelsLikeTemperatureC = undefined; //weatherJson.current.feelslike_c
 	this.feelsLikeTemperatureF = undefined; //weatherJson.current.feelslike_f
@@ -23,30 +23,48 @@ function CurrentWeather(weatherData){
 	// Rain chance
 	this.rainChance = undefined; //weatherJson.forecast.forecastDay[0].day.daily_chance_of_rain
 	// Wind speed
-    this.windSpeed = undefined; //weatherJson.current.wind_kph
+	this.windSpeed = undefined; //weatherJson.current.wind_kph
 }
 
 // Forecast object
-function ForecastDay(weatherData){
-    this.date = undefined; //weatherJson.forecast.forecastday.date
-    this.dayOfWeek = undefined; //weatherJson.forecast.forecastday.date (convert to day of the week)
-    this.temperatureC = undefined; //weatherJson.forecast.forecastday.day.avgtemp_c
-    this.temperatureCMin = undefined; //weatherJson.forecast.forecastday.day.mintemp_c
-    this.temperatureF = undefined; //weatherJson.forecast.forecastday.day.avgtemp_f
-    this.temperatureFMin = undefined; //weatherJson.forecast.forecastday.day.mintemp_f
-    this.weatherIcon = undefined; //weatherJson.forecast.forecastday.day.condition.icon
+function ForecastDay(weatherData) {
+	this.date = undefined; //weatherJson.forecast.forecastday.date
+	this.dayOfWeek = undefined; //weatherJson.forecast.forecastday.date (convert to day of the week)
+	this.temperatureC = undefined; //weatherJson.forecast.forecastday.day.avgtemp_c
+	this.temperatureCMin = undefined; //weatherJson.forecast.forecastday.day.mintemp_c
+	this.temperatureF = undefined; //weatherJson.forecast.forecastday.day.avgtemp_f
+	this.temperatureFMin = undefined; //weatherJson.forecast.forecastday.day.mintemp_f
+	this.weatherIcon = undefined; //weatherJson.forecast.forecastday.day.condition.icon
 }
 
-async function fetchWeather(location) {
+async function fetchWeather(location = 'London') {
 	// Call WeatherAPI to get the data from the specific location
-	// Request URL example: http://api.weatherapi.com/v1/current.json
-	// Code 200 (All good)
-	// Code 400
-	// Error code 1003: Parameter 'q' not provided.
-	// Error code 1005: API request url is invalid.
-	// Error code 1006: No location found matching parameter 'q'
+	// Request URL example:
+	// http://api.weatherapi.com/v1/forecast.json?key={API_KEY}&q={location}&days=3
+	const apiKey = '1f942dd367ca473d889171547241705';
+	const url = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=3`;
 
-	//show data in console
+    // fetch and check response status
+	try {
+		const response = await fetch(url);
 
-	//show data in dome
+		if (!response.ok) {
+			throw new Error(
+				`Error in fetchWeather(${location}), response status: ${response.status}, ${response.statusText}`
+			);
+		}
+        else {
+            const weatherData = await response.json();
+            return weatherData;
+        }
+
+	} catch (error) {
+        console.log(error);
+        return Promise.reject(error);
+    }
 }
+
+let weatherData = fetchWeather();
+weatherData.then((object)=>{
+    console.log(object);
+});
